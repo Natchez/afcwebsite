0ef017d14c4668b7919e0dafaa4bdc4d562d246e
Aaron Fields-Cypress <natchezdesigns@gmail.com>
2025-07-14 08:07:35 -0400

    material menu widths
    adjust widths of mat menu to match button size

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,13 @@
-import {Component} from '@angular/core';
-import {MatMenuModule} from '@angular/material/menu';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef, QueryList,
+  ViewChild, ViewChildren
+} from '@angular/core';
+import {MatMenuModule, MatMenuTrigger} from '@angular/material/menu';
 import {RouterOutlet} from '@angular/router';
+
+// import {NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +15,40 @@ import {RouterOutlet} from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements AfterViewInit {
+
+  protected panelWidth = 0;
   protected title = 'afcwebsite';
+
+  @ViewChildren('myTriggerBtn') allTriggerBtns!: QueryList<ElementRef<HTMLButtonElement>>;
+  @ViewChildren(MatMenuTrigger) menuTriggers!: QueryList<MatMenuTrigger>;
+
+  ngAfterViewInit() {
+    this.menuTriggers
+      .forEach((trigger, i) => {
+        trigger.menuOpened.subscribe(() => {
+
+          // Grab all Btns width if they exist
+          const btn = this.allTriggerBtns.get(i)?.nativeElement;
+          if (!btn) return;
+
+          const width = btn.offsetWidth;
+          console.log(width);
+
+          // Delay calculation so menu can load
+          setTimeout(() => {
+            const panes = document.querySelectorAll('.mat-mdc-menu-content');
+            const pane = panes[panes.length - 1] as HTMLElement;
+
+            if (pane) {
+              pane.style.width = `${width}px`;
+            } else {
+              console.log('not found');
+            }
+
+          }, 0);
+        });
+      });
+  }
 }
+
